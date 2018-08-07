afcbe065ea8b3c274fb5c01721ffd3ff76dc4483
Егор <35236837+Far-East@users.noreply.github.com>
2018-08-07 22:09:48 +0300

    Create active-link.js
    Подсветка активной ссылки jQery

---
+++ active-link.js
@@ -0,0 +1,12 @@
+// Подсветка активной ссылки jQery
+jQuery(document).ready(function($) { 
+
+	$('.main-nav li a').each(function () {
+	        var location = window.location.href;
+	        var link = this.href; 
+	        if(location == link) {
+	            $(this).addClass('active-item');
+	        }
+	});
+
+});
